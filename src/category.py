e2fbf5b126c252aa5e5469bb9946a701d8628b4c
Churilov Evgeny <i@churilovevgeny.ru>
2024-03-25 16:30:56 +0300

    Add in Category counter of categories and counter of products

--- src/category.py
+++ src/category.py
@@ -2,10 +2,20 @@ from src.product import Product
 
 
 class Category:
+    categories_count = 0
+    products_unique_count = 0
+
     def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
         self.products = list(
             products)  # Копия, чтобы не было непредвиденного поведения с доступом по ссылке к исходному списку
 
+        Category.categories_count += 1
 
+        # Реализация подсчета количества уникальных продуктов несколько спорная, так как в теории
+        # в одной категории могут быть несколько объектов Product с одинаковыми позициями
+        # или же в разные категории могут попасть одинаковые Product
+        # если такая возможность реальна, то, возможно, название Product надо хранить в set,
+        # но пока так
+        Category.products_unique_count += len(self.products)
