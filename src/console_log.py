09558472f63f3faaa07eec46f61d814dd32490c5
Churilov Evgeny <i@churilovevgeny.ru>
2024-04-11 21:15:43 +0300

    Remove commented code

--- src/console_log.py
+++ src/console_log.py
@@ -1,5 +1,4 @@
 class MixinConsoleLog:
 
     def __repr__(self):
-        # return f"{self.__class__.__name__}"
         return f"{self.__class__.__name__}({", ".join(map(str, self.__dict__.values()))})"
